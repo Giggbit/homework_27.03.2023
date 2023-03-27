7948f7ea642ca5b960d61319c34dae79a3fde9c2
Giggbit <danya19981017@gmail.com>
2023-03-27 20:45:56 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Декоратор_homework_27._03._2023
+{
+    abstract class Person {
+        public string name;    
+        public int attack;
+        public int speed;
+        public int health;
+        public int protection;
+
+        abstract public void Print();
+    }
+
+    class Human : Person {
+        public override void Print() {
+            name = "Human";
+            attack = 20;
+            speed = 20;
+            health = 150;
+            protection = 0;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack}");
+            Console.WriteLine($"Speed: {speed}");
+            Console.WriteLine($"Health: {health}");
+            Console.WriteLine($"Protection: {protection}");
+        }
+    }
+
+    class Elf : Person {
+        public override void Print() {
+            name = "Elf";
+            attack = 15;
+            speed = 30;
+            health = 100;
+            protection = 0;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack}");
+            Console.WriteLine($"Speed: {speed}");
+            Console.WriteLine($"Health: {health}");
+            Console.WriteLine($"Protection: {protection}");
+        }
+    }
+
+
+    class Decorator : Person {
+        protected Person person;
+
+        public Decorator(Person person) { 
+            this.person = person;
+        }
+        public void SetPerson(Person person) { 
+            this.person = person;
+        }
+        public override void Print() {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack}");
+            Console.WriteLine($"Speed: {speed}");
+            Console.WriteLine($"Health: {health}");
+            Console.WriteLine($"Protection: {protection}");
+        }
+    }
+
+    class HumanWarrior : Decorator {
+        public HumanWarrior(Person person) : base(person) {
+            this.person = person;
+        }
+        public override void Print() {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack += 20}");
+            Console.WriteLine($"Speed: {speed += 10}");
+            Console.WriteLine($"Health: {health += 50}");
+            Console.WriteLine($"Protection: {protection += 20}");
+        }
+    }
+    class Swordsman : Decorator {
+        public Swordsman(Person person) : base(person) {
+            this.person = person;
+        }
+        public override void Print() {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack += 40}");
+            Console.WriteLine($"Speed: {speed -= 10}");
+            Console.WriteLine($"Health: {health += 50}");
+            Console.WriteLine($"Protection: {protection += 40}");
+        }
+    }
+    class Archer : Decorator {
+        public Archer(Person person) : base(person) {
+            this.person = person;
+        }
+        public override void Print() {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack}");
+            Console.WriteLine($"Speed: {speed}");
+            Console.WriteLine($"Health: {health}");
+            Console.WriteLine($"Protection: {protection}");
+        }
+    }
+    class Rider : Decorator {
+        public Rider(Person person) : base(person) {
+            this.person = person;
+        }
+        public override void Print() {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack}");
+            Console.WriteLine($"Speed: {speed}");
+            Console.WriteLine($"Health: {health}");
+            Console.WriteLine($"Protection: {protection}");
+        }
+    }
+    class ElfWarrior : Decorator {
+        public ElfWarrior(Person person) : base(person) {
+            this.person = person;
+        }
+        public override void Print() {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack}");
+            Console.WriteLine($"Speed: {speed}");
+            Console.WriteLine($"Health: {health}");
+            Console.WriteLine($"Protection: {protection}");
+        }
+    }
+    class ElfMage : Decorator {
+        public ElfMage(Person person) : base(person) {
+            this.person = person;
+        }
+        public override void Print() {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack}");
+            Console.WriteLine($"Speed: {speed}");
+            Console.WriteLine($"Health: {health}");
+            Console.WriteLine($"Protection: {protection}");
+        }
+    }
+    class Crossbowelf : Decorator {
+        public Crossbowelf(Person person) : base(person) {
+            this.person = person;
+        }
+        public override void Print() {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack}");
+            Console.WriteLine($"Speed: {speed}");
+            Console.WriteLine($"Health: {health}");
+            Console.WriteLine($"Protection: {protection}");
+        }
+    }
+    class EvilMage : Decorator {
+        public EvilMage(Person person) : base(person) {
+            this.person = person;
+        }
+        public override void Print() {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack}");
+            Console.WriteLine($"Speed: {speed}");
+            Console.WriteLine($"Health: {health}");
+            Console.WriteLine($"Protection: {protection}");
+        }
+    }
+    class GoodMage : Decorator {
+        public GoodMage(Person person) : base(person) {
+            this.person = person;
+        }
+        public override void Print() {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Attack: {attack}");
+            Console.WriteLine($"Speed: {speed}");
+            Console.WriteLine($"Health: {health}");
+            Console.WriteLine($"Protection: {protection}");
+        }
+    }
+
+
+    class Client { 
+        public void SetPerson(Person person) {
+            person.Print();
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            
+            var a = new Human();
+            client.SetPerson(a);
+
+            Console.WriteLine();
+
+            HumanWarrior hw = new HumanWarrior(a);
+            client.SetPerson(hw);
+
+            Console.WriteLine();
+
+            Swordsman sw = new Swordsman(a);
+            client.SetPerson(sw);
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Декоратор homework 27.03.2023")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Декоратор homework 27.03.2023")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7a18c391-d275-4f81-822b-e4b4dcbed00d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
